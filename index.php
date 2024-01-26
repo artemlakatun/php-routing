5cb209304806d815ebb2ca4e331ed4fc0786b8a1
Artem_Lakatun <alakatuni@mail.ru>
2024-01-26 19:27:37 +0300

    feat: text URL-routing

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$request = $_SERVER['REQUEST_URI'];
+$viewDir = '/views/';
+
+switch ($request) {
+    case '/':
+        require __DIR__ . $viewDir . 'home.php';
+        break;
+    case '/users':
+        require __DIR__ . $viewDir . 'users.php';
+        break;
+    case '/contact':
+        require __DIR__ . $viewDir . 'contact.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . $viewDir . '404.php';
+}
